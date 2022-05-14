a560de687557bbc6aa6fcb8875358dc615540ea3
hokageCV <chaitanyavaru@gmail.com>
2022-05-14 13:59:45 +0530

    initial commit

---
+++ library.js
@@ -0,0 +1,6 @@
+// plan is to make a book tracker, with local storage
+// TWO kinds of Input methods should be there
+//         1) directly from the screen
+//                like https://ginnerzapata.github.io/library-app/
+//         2) from an pop up box
+//                like https://sultanbadri.github.io/virtual-library/
